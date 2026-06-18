a7eb8e6b72fd218cb0512d24b85adba66509548a
JosepRivera <josepdanton1518@gmail.com>
2026-06-18 17:55:02 -0500

    Initial Commit

---
+++ tecstore-manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tecstore-manager
+//
+//  Created by Josep D. Rivera on 18/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
